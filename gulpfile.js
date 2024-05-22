ae5bdcc4f6801d40325426206e9f8c511da1f2d4
Andre Pavan <dehnuernberg@hotmail.com>
2024-05-21 22:33:58 -0300

    criando o hero e importanto as fontes Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // importar os arquivos
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 // funcao criada para comprimir o css
 function styles(){
@@ -9,7 +10,13 @@ function styles(){
         .pipe(gulp.dest('./dist/css')); // adiciona em uma pasta nova dist/css
 }
 
-exports.default = styles; // funcao default
+function images(){
+    return gulp.src('./src/images/**/*') // pega todos arquivos de todas as pastas em images
+        .pipe(imagemin()) // commprime os arquivos
+        .pipe(gulp.dest('./dist/images')); // adiciona em uma pasta nova dist/css
+}
+
+exports.default = gulp.parallel(styles, images); // funcao default
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles)); // adicionado o watcher para os arquivos de estilo
     // watcher é responsável por salvar e compilar direto sem ter que usar o npm run
